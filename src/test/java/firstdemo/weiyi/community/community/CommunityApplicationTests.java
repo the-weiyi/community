aff45688110f14864b6b50b644cfcd0f4bf5211a
weiyi <2913376014@qq.com>
2020-12-28 09:53:55 +0800

    first project

---
+++ src/test/java/firstdemo/weiyi/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package firstdemo.weiyi.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
